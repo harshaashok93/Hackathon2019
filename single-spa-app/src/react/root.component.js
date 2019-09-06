c42e20a52387748aa6651b343c4772ba4286d046
Harsha Ashok <harshaashok93@gmail.com>
2019-09-06 13:48:15 +0530

    Hackathon setup

---
+++ single-spa-app/src/react/root.component.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello</h1>
+            <button><a href="/react">React</a></button>
+            <button><a href="/vue">Vue</a></button>
+        </div>
+    )
+}
+
+export default App
---
+++ single-spa-app/src/vue/main.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+      <h1>Hello from Vue</h1>
+      <button><a href="/react">React</a></button>
+      <button><a href="/vue">Vue</a></button>
+  </div>
+</template>
